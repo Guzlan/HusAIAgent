1a1ad6ccf5fb67ed866eabbb8737d5b959228fec
Guzlan <amro.guzlan@gmail.com>
2016-03-30 00:22:47 -0400

    Alpha beta pruning working

--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -5,6 +5,7 @@ package student_player;
 import hus.HusBoardState;
 import hus.HusPlayer;
 import hus.HusMove;
+import hus.HusMove.MoveType;
 
 import java.util.ArrayList;
 
@@ -17,33 +18,81 @@ public class StudentPlayer extends HusPlayer {
      * This is important, because this is what the code that runs the
      * competition uses to associate you with your agent.
      * The constructor should do nothing else. */
-    public StudentPlayer() { super("xxxxxxxxx"); }
+    public StudentPlayer() { super("260553379"); }
 
     /** This is the primary method that you need to implement.
      * The ``board_state`` object contains the current state of the game,
      * which your agent can use to make decisions. See the class hus.RandomHusPlayer
      * for another example agent. */
-    public HusMove chooseMove(HusBoardState board_state)
+  public HusMove chooseMove(HusBoardState board_state)
     {
-        // Get the contents of the pits so we can use it to make decisions.
-        int[][] pits = board_state.getPits();
-
-        // Use ``player_id`` and ``opponent_id`` to get my pits and opponent pits.
-        int[] my_pits = pits[player_id];
-        int[] op_pits = pits[opponent_id];
-
-        // Use code stored in ``mytools`` package.
-        MyTools.getSomething();
+    	
+    	if(board_state.getTurnNumber()==0) return new HusMove(8);
 
         // Get the legal moves for the current board state.
         ArrayList<HusMove> moves = board_state.getLegalMoves();
-        HusMove move = moves.get(0);
-
-        // We can see the effects of a move like this...
-        HusBoardState cloned_board_state = (HusBoardState) board_state.clone();
-        cloned_board_state.move(move);
-
-        // But since this is a placeholder algorithm, we won't act on that information.
+        HusMove move= null;
+        int score = Integer.MIN_VALUE; // start with having score of minus infinity. 
+        for (HusMove tempMove : moves ){
+        	if(tempMove.getPlayerID()==player_id){
+        		HusBoardState decisionBoard = (HusBoardState) board_state.clone();
+        		decisionBoard.move(tempMove);
+        		int temp = miniMax_AlphaBeta(5,player_id,opponent_id,false,Integer.MIN_VALUE, Integer.MAX_VALUE, decisionBoard);
+        		if ( temp > score){
+        			score = temp; 
+        			move = tempMove;
+        		}
+        	}
+        }
         return move;
+    }
+    
+    public int miniMax_AlphaBeta(int level,int player_id, int opponent_id, boolean maximizingPlayer, int alpha, int beta, HusBoardState boardState){
+    	if (level == 0 || boardState.gameOver()) { // in case we reach the depth of our search tree of if the game is over. 
+    		return evaluationFunction(boardState);  
+    	}
+    	if (maximizingPlayer){ // my player / computer's turn to play now
+    		int score = Integer.MIN_VALUE;
+			ArrayList<HusMove> moves = boardState.getLegalMoves();
+			for (HusMove move : moves){
+				HusBoardState decisionBoard = (HusBoardState) boardState.clone(); // cloning board to see effect. 
+				if (move.getPlayerID()==player_id && decisionBoard.move(move)){  
+    				score =Math.max(score, miniMax_AlphaBeta(level-1,player_id,opponent_id, false, alpha,beta,decisionBoard));
+    				alpha = Math.max(alpha, score); 
+    				if(alpha>=beta) break ; // cut off for beta. pruning happens here. 
+    			}
+    		}
+    		return score;
+    	}
+    	else { //mimic opponents play 
+    		int score = Integer.MAX_VALUE;
+			ArrayList<HusMove> moves = boardState.getLegalMoves();
+			for (HusMove move : moves){
+				HusBoardState decisionBoard = (HusBoardState) boardState.clone(); // cloning board to see effect. 
+    			if (move.getPlayerID()==opponent_id && decisionBoard.move(move)){ 
+    				score =Math.min(score, miniMax_AlphaBeta(level-1,player_id,opponent_id, true, alpha,beta,decisionBoard));
+    				beta = Math.min(beta, score); 
+    				if(alpha>=beta) break ; // cut off for alpha. pruning happens here. 
+    			}
+    		}
+    		return score;
+    	}
+    }
+   
+    public int evaluationFunction(HusBoardState boardState){
+    	// initial evaluation function is to add all the number of seeds in pits with larger than 1 seed of myAgent 
+    	// subtract from it the sum of the number of seeds in pits that have larger than 1 of enemy. 
+    	int[][] pits = boardState.getPits();
+        int[] my_pits = pits[player_id];
+        int[] op_pits = pits[opponent_id];
+        int myScore=0; 
+        int opScore=0;
+        
+        for (int i= 0 ; i <16; i++) {
+        	if(my_pits[i] >1 ) myScore+=my_pits[i];
+        	if(op_pits[i]>1) opScore += op_pits[i];
+        }
+        
+    	return myScore-opScore; 
     }
 }
--- src/student_player/mytools/MyTools.java
+++ src/student_player/mytools/MyTools.java
@@ -1,8 +1,25 @@
 package student_player.mytools;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ArrayDeque;
+
 
 public class MyTools {
-
     public static double getSomething(){
         return Math.random();
     }
+    public static int[][] getPitScores(int [] enemyPits){  
+    	int [][] scores = new int[16][1];
+    	int index = 0; 
+    	for (int i = 16; i < 32; i++){
+    		scores[index][0] = enemyPits[i] >0 ? enemyPits[i]+enemyPits[31-i] : -1;
+    	}
+    	Arrays.sort(scores, new java.util.Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Double.compare(a[0], b[0]);
+            }
+        });
+    	return scores; 
+    }
+    
 }
